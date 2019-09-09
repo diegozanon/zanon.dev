cb895f203c851d7a9cb4dafa17deb8b71d1bbef0
diegozanon <12113544+diegozanon@users.noreply.github.com>
2019-09-09 00:21:48 -0300

    Starting the single page application

---
+++ site/js/spa.ts
@@ -0,0 +1,34 @@
+const anchors = document.getElementsByTagName('a');
+
+for (const anchor of anchors) {
+    const href = anchor.getAttribute('href');
+
+    // handle only local links
+    if (href.startsWith('/')) {
+        anchor.addEventListener('click', (evt): void => {
+
+            // if the user is trying to open in a new tab, do nothing
+            // metaKey is apple command
+            // evt button = 1 is middle mouse button
+            if (evt.ctrlKey || evt.shiftKey || evt.metaKey || (evt.button && evt.button === 1)) {
+                return;
+            }
+
+            // else, prevent default behavior
+            evt.preventDefault();
+
+            fetch('/pages/data.json')
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error("HTTP error " + response.status);
+                    }
+
+                    return response.json();
+                })
+                .then(json => {
+                    console.info(json); // do something
+                })
+                .catch(console.error);
+        });
+    }
+}
