aaf515b0aae5568eb39e334b604cb22f601569fb
diegozanon <12113544+diegozanon@users.noreply.github.com>
2020-10-08 23:44:08 -0300

    Fetching preloaded jsons

--- site/js/spa.ts
+++ site/js/spa.ts
@@ -1,5 +1,35 @@
-const anchors = document.getElementsByTagName('a');
+import { PostsJson, Page, Post } from './types';
 
+async function fetchJson<T>(filename: string): Promise<T> {
+
+    const response = await fetch(filename);
+
+    if (!response.ok)
+        throw `HTTP error retrieving ${filename}: ${response.status}`;
+
+    return await response.json() as T;
+}
+
+let postsJson: PostsJson;
+let siteJson: Page[];
+
+(async (): Promise<void> => {
+
+    [postsJson, siteJson] = await Promise.all([
+        fetchJson<PostsJson>('/posts.json'),
+        fetchJson<Page[]>('/site.json')]
+    );
+
+})().catch(console.error);
+
+const switchSitePage = (targetLink: string): void => {
+}
+
+const switchPostPage = (targetLink: string): void => {
+
+}
+
+const anchors = document.getElementsByTagName('a');
 for (const anchor of anchors) {
     const href = anchor.getAttribute('href');
 
@@ -10,25 +40,25 @@ for (const anchor of anchors) {
             // if the user is trying to open in a new tab, do nothing
             // metaKey is apple command
             // evt button = 1 is middle mouse button
-            if (evt.ctrlKey || evt.shiftKey || evt.metaKey || (evt.button && evt.button === 1)) {
+            if (evt.ctrlKey || evt.shiftKey || evt.metaKey || evt.button === 1) {
                 return;
             }
 
-            // else, prevent default behavior
-            evt.preventDefault();
+            const sitePages = siteJson && siteJson.map((site: Page) => { return site.slug });
+            const postsPages = postsJson && postsJson.posts.map((post: Post) => { return post.header.slug });
 
-            fetch('/posts.json') // it is probably cached already
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error("HTTP error " + response.status);
-                    }
-
-                    return response.json();
-                })
-                .then(json => {
-                    console.info(json); // do something
-                })
-                .catch(console.error);
+            if (sitePages.includes(href)) {
+                evt.preventDefault();
+                switchSitePage(href);
+            } else if (postsPages.includes(href)) {
+                evt.preventDefault();
+                switchPostPage(href);
+            } else if (siteJson) { // 404
+                evt.preventDefault();
+                switchSitePage(href);
+            } else {
+                return;
+            }
         });
     }
 }
--- site/js/types.ts
+++ site/js/types.ts
@@ -8,3 +8,27 @@ export enum Theme {
     Light = 'light-theme',
     Dark = 'dark-theme'
 }
+
+export interface PostsJson {
+    posts: Array<Post>;
+    template: string;
+}
+
+export interface Post {
+    header: PostMeta;
+    html: string;
+}
+
+export interface PostMeta {
+    date: string;
+    slug: string;
+    title: string;
+    description: string;
+    thumbnail: string;
+    tags: Array<string>;
+}
+
+export interface Page {
+    slug: string;
+    html: string;
+}
