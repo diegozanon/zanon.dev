3aedc4294f03bbe8c34ba3ffbae05f1481527c66
diegozanon <12113544+diegozanon@users.noreply.github.com>
2020-10-18 10:35:16 -0300

    Lambda: downloading the updated project and running the build command

--- src/deploy/handler.ts
+++ src/deploy/handler.ts
@@ -1,8 +1,35 @@
-import { promisify } from 'util';
 import * as childProcess from 'child_process';
-
-const exec = promisify(childProcess.exec);
+import { download, DownloadOptions } from 'get-github-code';
+import * as fse from 'fs-extra';
 
 export const deploy = async (): Promise<void> => {
-    const { stdout, stderr } = await exec('npm run build');
+
+    const output = './downloaded';
+
+    // dowload the updated project
+    const options: DownloadOptions = {
+        username: 'diegozanon',
+        repo: 'zanon.dev',
+        branch: 'feature/r01/deploy',
+        output
+    };
+
+    await download(options);
+
+    await fse.copy('./node_modules', `${output}/node_modules`);
+
+    const args = `run build --prefix ${output}`.split(' ');
+    await new Promise((resolve, reject) => {
+        const npmRunBuild = childProcess.spawn('npm', args);
+
+        npmRunBuild.stdout.on('data', () => {
+            // console.info(`Build: ${data}`); // log only if debug is needed
+        });
+
+        npmRunBuild.stderr.on('data', reject);
+
+        npmRunBuild.on('close', code => {
+            code == 0 ? resolve() : reject(code);
+        });
+    });
 }
