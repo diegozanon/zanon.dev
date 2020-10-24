c5d472b3cb5bdcaaeed58120cf7bed73d02b6da1
diegozanon <12113544+diegozanon@users.noreply.github.com>
2020-10-24 18:22:34 -0300

    Adding a GET resource for comments

--- src/backend/handler.ts
+++ src/backend/handler.ts
@@ -1,18 +1,27 @@
 import { APIGatewayProxyResult, APIGatewayProxyEvent } from 'aws-lambda';
 import { successHandler, errorHandler } from '../common/http-response';
 import { BackendRequestType } from '../common/types';
-import { registerComment } from './lib/comment';
+import { getComments, registerComment } from './lib/comment';
 import { registerFeedback } from './lib/feedback';
 import { registerVisit } from './lib/visit';
 
 export const backend = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
-    if (event.httpMethod === 'OPTIONS') {
-        return successHandler({ message: 'success', cors: true, httpMethod: 'OPTIONS' });
-    }
-
     try {
+        if (event.httpMethod === 'OPTIONS') {
+            return successHandler({ message: 'success', cors: true, httpMethod: 'OPTIONS' });
+        }
+
         const body = JSON.parse(event.body);
+
+        if (event.httpMethod === 'GET') {
+
+            if (body.requestType === BackendRequestType.Comment) {
+                return await getComments(body);
+            } else {
+                return errorHandler({ error: new Error('Invalid request type'), cors: true })
+            }
+        }
 
         switch (body.requestType) {
             case BackendRequestType.Comment:
--- src/backend/lib/comment.ts
+++ src/backend/lib/comment.ts
@@ -1,3 +1,10 @@
+import { APIGatewayProxyResult } from "aws-lambda";
+import { successHandler } from "../../common/http-response";
+
+export const getComments = async (obj: object): Promise<APIGatewayProxyResult> => {
+    return successHandler({ data: obj, cors: true });
+}
+
 export const registerComment = async (obj: object): Promise<void> => {
     console.info(obj);
 }
