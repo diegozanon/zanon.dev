1339d03f996777279b9c09837d7fc0c822d4eae5
diegozanon <12113544+diegozanon@users.noreply.github.com>
2021-04-02 18:57:47 -0300

    Updating /me

--- src/gulp/build-html.ts
+++ src/gulp/build-html.ts
@@ -34,7 +34,7 @@ export const copyToDist = async (done): Promise<void> => {
     await fse.copy('site/favicon.ico', 'site/dist/favicon.ico');
     await fse.copy('site/manifest.json', 'site/dist/manifest.json');
     await fse.copy('site/robots.txt', 'site/dist/robots.txt');
-    await fse.copy('site/resources/cv.pdf', 'site/dist/resources/cv.pdf');
+    await fse.copy('site/resources/resume.pdf', 'site/dist/resources/resume.pdf');
 
     done();
 }
